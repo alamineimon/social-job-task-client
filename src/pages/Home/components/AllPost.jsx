8a267958978089c9fe333b71e004fb82831c12b3
Al Amin Eimon <eimonalamin@gmail.com>
2023-03-12 10:34:16 +0600

    implement allpost dynamic from mongodb

--- src/pages/Home/components/AllPost.jsx
+++ src/pages/Home/components/AllPost.jsx
@@ -1,11 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 import { PostsData } from '../../../assets/Data/PostsData'
 import PostCard from './PostCard'
 
 const AllPost = () => {
+
+  const { data: allpost, isLoading } = useQuery({
+    queryKey: ["allpost"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:9000/allpost", {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+      return data;
+    },
+  });
   return (
-    <div className="flex-col gap-4">
-    {PostsData.map((post, id)=>{
+    <div className="flex-col gap-6">
+    {allpost?.map((post, id)=>{
         return <PostCard data={post} id={id}/>
     })}
 </div>
--- src/pages/Home/components/PostCard.jsx
+++ src/pages/Home/components/PostCard.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import Comment from '../../../assets/img/comment.png'
-import Share from '../../../assets/img/share.png'
-import Heart from '../../../assets/img/like.png'
-import NotLike from '../../../assets/img/notlike.png'
+import React from "react";
+import Comment from "../../../assets/img/comment.png";
+import Share from "../../../assets/img/share.png";
+import Heart from "../../../assets/img/like.png";
+import NotLike from "../../../assets/img/notlike.png";
 
-
-const PostCard = ({data}) => {
+const PostCard = ({ data }) => {
   return (
-    <div className="flex-col p-4 rounded-md static getDerivedStateFromProps(nextProps, prevState) {}">
-    <img src={data.img} alt="" className='w-full max-h-80 object-cover rounded-sm' />
-    <div className="flex items-start gap-6 mt-3">
-        <img src={data.liked===true?Heart: NotLike} alt="" className='cursor-pointer'/>
-        <img src={Comment} alt="" className='cursor-pointer' />
-        <img src={Share} alt=""  className='cursor-pointer'/>
-    </div>
-    <span style={{color: "var(--gray)", fontSize: '12px'}}>{data.likes} likes</span>
-    <div className="detail">
-        <span><b>{data.name}</b></span>
+    <div className="flex-col mb-6 p-4 bg-white rounded-md static getDerivedStateFromProps(nextProps, prevState) {}">
+      <img
+        src={data.img}
+        alt=""
+        className="w-full max-h-80 object-cover rounded-sm"
+      />
+      <div className="flex items-start gap-6 mt-3">
+        <img
+          src={data.liked === true ? Heart : NotLike}
+          alt=""
+          className="cursor-pointer"
+        />
+        <img src={Comment} alt="" className="cursor-pointer" />
+        <img src={Share} alt="" className="cursor-pointer" />
+      </div>
+      <span className="text-gray-500">{data.likes} likes</span>
+      <div className="detail text-gray-500">
+        <span className="text-black font-bold text-md">{data.name}</span>
         <span> {data.desc}</span>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
